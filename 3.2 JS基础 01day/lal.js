6caa07689066fc01bc542f9fffe44a2056f94386
570805869 <570805869@qq.com>
2018-04-14 14:48:27 +0800

    Initial commit

---
+++ 3.2 JS基础 01day/lal.js
@@ -0,0 +1,30 @@
+// alert('123');
+var age = 123;
+var str = 123, str1 = 'xudi', str3 = 'liumeijun', str2 = true;
+var tmp;
+var s1 = 123.4332;
+
+console.log(Number(str));
+console.log(parseInt(s1));
+console.log(parseFloat(s1));
+console.log()
+
+// console.log(typeof str);
+// console.log(typeof str1);
+// console.log(typeof str2);
+// console.log(typeof str.toString());
+// console.log(String(str));
+// console.log(typeof(true + ''));
+
+
+// tmp = str1;
+// str1 = str;
+// str = tmp;
+// tmp = null;
+// console.log(str1,str,tmp);
+
+// console.log(1+1);
+// console.log(2+'4');
+// console.log(2+'5'*3);
+// console.log(4+5+'5'*3);
+// console.log('5'*2+4+'3'+4+'2'*2);
